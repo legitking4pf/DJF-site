5b7459a79e390b7d669182ef00c5942290c74510
legitking4pf <ayewumijustice123@gmail.com>
2026-01-24 10:56:05 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,36 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
     return [
-    {
-      allowedDevOrigins: ['10.161.32.3', 'localhost:3000'],
-      reactCompiler: true,
-      source: '/(.*)',
-      headers: [
-        { key: 'Content-Security-Policy',
-          value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';" },
-        { key: 'X-Frame-Options', value: 'DENY' },
-        { key: 'X-Content-Type-Options', value: 'nosniff' },
-        { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-      ],
-    }, ];
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: hv4w1qmfjrk8zaij.public.blob.vercel-storage.com bancatlan.hn images.unsplash.com; font-src 'self' data:; connect-src 'self';"
+              .replace(/\s{2,}/g, ' ')
+              .trim(),
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
   },
 };
+
 export default nextConfig;
