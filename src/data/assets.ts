6adf2721eccba97a579d652d0f6721149138e9dc
legitking4pf <ayewumijustice123@gmail.com>
2026-01-26 12:48:38 +0100

    update assets.ts

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -1,4 +1,9 @@
-export const assets = [
+type Asset = {
+  name: string;
+  coords: [number, number];
+};
+
+export const assets: Asset[] = [
   { name: "Honduras", coords: [15.2, -86.2] },
   { name: "Spain", coords: [40.4, -3.7] },
-];
+]
