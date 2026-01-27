22e6b2510010406d5f62617bfda15244b036f54a
legitking4pf <ayewumijustice123@gmail.com>
2026-01-28 00:55:25 +0100

    update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useForm, ValidationError } from "@formspree/react";
-import type { motion, AnimatePresence, Variants} from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import type { Variants } from "framer-motion";
 import {
   ShieldCheck,
   ArrowRight,
