202562188a9927e58b010633be274e4bb0733448
legitking4pf <ayewumijustice123@gmail.com>
2026-02-17 09:11:41 +0100

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Roboto_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ 
   subsets: ['latin'], 
   display: 'swap',
